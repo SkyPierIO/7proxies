491104d879ead74c23bf664921a29f740ce2e2a2
Luis Miguel <lgomez@rivian.com>
2023-09-23 17:11:04 -0500

    Update contract

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -4,84 +4,45 @@ pragma solidity >=0.8.0 <0.9.0;
 // Useful for debugging. Remove when deploying to a live network.
 import "hardhat/console.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
+contract HostContract is Ownable  {
+	// Events
+	event HostRegistered(address indexed host, string nodeId);
+	event HostUnregistered(address indexed host, string nodeId);
+	/**
+	* A smart contract that stores the hosts registered on the network
+	* @author Ting
+	*/
+	struct NodeInfo {
+		string nodeId;
+		uint256 balance;
+		bool active;
+		uint users;
+	}
 	// State Variables
-	address public immutable owner;
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
-	mapping(address => uint) public userGreetingCounter;
+	mapping(address host => NodeInfo info) hostsToInfo;
 
-	// Events: a way to emit log statements from smart contract that can be listened to by external parties
-	event GreetingChange(
-		address indexed greetingSetter,
-		string newGreeting,
-		bool premium,
-		uint256 value
-	);
-
-	// Constructor: Called once on contract deployment
-	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
+	function getHost(address host) public view returns (NodeInfo) {
+		return tokenIdToInfo[host];
 	}
 
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
-	}
-
-	/**
-	 * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-	 *
-	 * @param _newGreeting (string memory) - new greeting to save on the contract
-	 */
-	function setGreeting(string memory _newGreeting) public payable {
-		// Print data to the hardhat chain console. Remove when deploying to a live network.
-		console.log(
-			"Setting new greeting '%s' from %s",
-			_newGreeting,
-			msg.sender
-		);
-
-		// Change state variables
-		greeting = _newGreeting;
-		totalCounter += 1;
-		userGreetingCounter[msg.sender] += 1;
-
-		// msg.value: built-in global variable that represents the amount of ether sent with the transaction
-		if (msg.value > 0) {
-			premium = true;
-		} else {
-			premium = false;
+	function registerAsHost(string memory nodeId) public {
+		if(!hostsToInfo[msg.sender]){
+			hostsToInfo[msg.sender] = NodeInfo(nodeId, 0, true);
+			emit HostRegistered(msg.sender, nodeId);
 		}
-
-		// emit: keyword used to trigger an event
-		emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, 0);
 	}
 
-	/**
-	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
-	 */
-	function withdraw() public isOwner {
-		(bool success, ) = owner.call{ value: address(this).balance }("");
-		require(success, "Failed to send Ether");
+	function unregisterAsHost(string memory nodeId) public {
+		if(hostsToInfo[msg.sender]){
+			hostsToInfo[msg.sender].nodeInfo = "";
+			hostsToInfo[msg.sender].active = false;
+			emit HostUnregistered(msg.sender, nodeId);
+		}
 	}
 
-	/**
-	 * Function that allows the contract to receive ETH
-	 */
-	receive() external payable {}
+	function useHost(address host) public {
+		hostsToInfo.users++;
+	}
 }
