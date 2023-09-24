2d4a1bcd077c86ca965b97e3454a915ea286c104
Luis Miguel <lgomez@rivian.com>
2023-09-24 06:43:14 -0500

    Show host data

--- packages/nextjs/components/host/ContractData.tsx
+++ packages/nextjs/components/host/ContractData.tsx
@@ -1,19 +1,14 @@
 import { useEffect, useRef, useState } from "react";
-import { gql, useQuery } from "@apollo/client";
-import Marquee from "react-fast-marquee";
 import { useAccount } from "wagmi";
 import {
   useAnimationConfig,
   useScaffoldContract,
   useScaffoldContractRead,
-  useScaffoldEventHistory,
-  useScaffoldEventSubscriber,
 } from "~~/hooks/scaffold-eth";
 
 const MARQUEE_PERIOD_IN_SEC = 5;
 
-export const ContractData = () => {
-  const { address } = useAccount();
+export const ContractData = ({hostData}) => {
   const [transitionEnabled, setTransitionEnabled] = useState(true);
   const [isRightDirection, setIsRightDirection] = useState(false);
   const [marqueeSpeed, setMarqueeSpeed] = useState(0);
@@ -21,47 +16,12 @@ export const ContractData = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const greetingRef = useRef<HTMLDivElement>(null);
 
-  const { data: totalCounter } = useScaffoldContractRead({
-    contractName: "YourContract",
-    functionName: "totalCounter",
-  });
-
-  const { data: currentGreeting, isLoading: isGreetingLoading } = useScaffoldContractRead({
-    contractName: "YourContract",
-    functionName: "greeting",
-  });
-
-  useScaffoldEventSubscriber({
-    contractName: "YourContract",
-    eventName: "GreetingChange",
-    listener: logs => {
-      logs.map(log => {
-        const { greetingSetter, value, premium, newGreeting } = log.args;
-        console.log("📡 GreetingChange event", greetingSetter, value, premium, newGreeting);
-      });
-    },
-  });
-
-  const {
-    data: myGreetingChangeEvents,
-    isLoading: isLoadingEvents,
-    error: errorReadingEvents,
-  } = useScaffoldEventHistory({
-    contractName: "YourContract",
-    eventName: "GreetingChange",
-    fromBlock: process.env.NEXT_PUBLIC_DEPLOY_BLOCK ? BigInt(process.env.NEXT_PUBLIC_DEPLOY_BLOCK) : 0n,
-    filters: { greetingSetter: address },
-    blockData: true,
-  });
-
-  console.log("Events:", isLoadingEvents, errorReadingEvents, myGreetingChangeEvents);
-
   const { data: yourContract } = useScaffoldContract({ contractName: "YourContract" });
   console.log("yourContract: ", yourContract);
 
-  const { showAnimation } = useAnimationConfig(totalCounter);
+  const { showAnimation } = useAnimationConfig(hostData?.users);
 
-  const showTransition = transitionEnabled && !!currentGreeting && !isGreetingLoading;
+  const showTransition = transitionEnabled;
 
   useEffect(() => {
     if (transitionEnabled && containerRef.current && greetingRef.current) {
@@ -70,27 +30,6 @@ export const ContractData = () => {
       );
     }
   }, [transitionEnabled, containerRef, greetingRef]);
-
-  const GREETINGS_GRAPHQL = `
-  {
-    greetings(first: 25, orderBy: createdAt, orderDirection: desc) {
-      id
-      greeting
-      premium
-      value
-      createdAt
-      sender {
-        address
-        greetingCount
-      }
-    }
-  }
-  `;
-
-  const GREETINGS_GQL = gql(GREETINGS_GRAPHQL);
-  const greetingsData = useQuery(GREETINGS_GQL, { pollInterval: 1000 });
-
-  console.log("greetingsData: ", greetingsData);
 
   return (
     <div className="flex flex-col justify-center items-center bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] py-10 px-5 sm:px-0 lg:py-auto max-w-[100vw] ">
@@ -113,9 +52,9 @@ export const ContractData = () => {
             />
           </button>
           <div className="bg-secondary border border-primary rounded-xl flex">
-            <div className="p-2 py-1 border-r border-primary flex items-end">Total count</div>
+            <div className="p-2 py-1 border-r border-primary flex items-end">Total users</div>
             <div className="text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end font-bai-jamjuree">
-              {totalCounter?.toString() || "0"}
+              {hostData?.users || "0"}
             </div>
           </div>
         </div>
@@ -124,23 +63,7 @@ export const ContractData = () => {
           <div className="relative overflow-x-hidden" ref={containerRef}>
             {/* for speed calculating purposes */}
             <div className="absolute -left-[9999rem]" ref={greetingRef}>
-              <div className="px-4">{currentGreeting}</div>
             </div>
-            {new Array(3).fill("").map((_, i) => {
-              const isLineRightDirection = i % 2 ? isRightDirection : !isRightDirection;
-              return (
-                <Marquee
-                  key={i}
-                  direction={isLineRightDirection ? "right" : "left"}
-                  gradient={false}
-                  play={showTransition}
-                  speed={marqueeSpeed}
-                  className={i % 2 ? "-my-10" : ""}
-                >
-                  <div className="px-4">{currentGreeting || " "}</div>
-                </Marquee>
-              );
-            })}
           </div>
         </div>
 
