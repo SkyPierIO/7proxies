dc09c397c36007c3758f6a12d2192c29ace7c135
smolgroot <nautilushell@protonmail.com>
2023-10-31 00:32:12 +0100

    Customize node dashboard

--- packages/nextjs/components/host/ContractInteraction.tsx
+++ packages/nextjs/components/host/ContractInteraction.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ArrowSmallRightIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ChartPieIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 export const ContractInteraction = ({nodeId}) => {
@@ -8,8 +8,11 @@ export const ContractInteraction = ({nodeId}) => {
       <div className="flex flex-col w-full mx-5 sm:mx-8 2xl:mx-20">
 
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Welcome host</span>
-          <span className="text-1xl text-grey">Node Id: {nodeId}</span>
+          <h2 className="text-5xl sm:text-5xl text-black flex mb-5">
+            <ChartPieIcon className="h-11 w-11 mr-3"/>
+            My node dashboard
+          </h2>
+          <span className="text-1xl text-grey"><strong>Peer ID</strong>: <code>{nodeId}</code></span>
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
               <div className="flex rounded-full border-2 border-primary p-1">
